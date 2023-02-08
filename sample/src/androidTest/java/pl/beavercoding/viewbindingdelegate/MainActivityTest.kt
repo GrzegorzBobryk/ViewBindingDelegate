f92555360a7d8948435a8eef233ef155de1fe5a5
Grzesiek <g.bobryk@gmail.com>
2023-02-08 15:33:22 +0100

    espresso test added to check sample app integration with delegate

---
+++ sample/src/androidTest/java/pl/beavercoding/viewbindingdelegate/MainActivityTest.kt
@@ -0,0 +1,108 @@
+package pl.beavercoding.viewbindingdelegate
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val resources = InstrumentationRegistry.getInstrumentation().targetContext.resources
+
+        val textView = onView(
+            allOf(
+                withId(R.id.hello_world), withText("Screen with ViewBinding"),
+                withParent(withParent(withId(R.id.nav_host_fragment))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText(resources.getString(R.string.example_1))))
+
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.button), withText("next"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_host_fragment),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val textView2 = onView(
+            allOf(
+                withId(R.id.hello_world), withText(resources.getString(R.string.example_2)),
+                withParent(withParent(withId(R.id.nav_host_fragment))),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("Screen with DataBinding")))
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.button), withText("next"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_host_fragment),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+
+        val textView3 = onView(
+            allOf(
+                withId(R.id.hello_world), withText(resources.getString(R.string.example_3)),
+                withParent(withParent(withId(R.id.nav_host_fragment))),
+                isDisplayed()
+            )
+        )
+        textView3.check(matches(withText("Screen with ViewBinding and something important in onCreateView")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent) && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
