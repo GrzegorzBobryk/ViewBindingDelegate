deb4fc215b5d0e40344076f32b25e6be4b87a1f5
Grzegorz Bobryk <g.bobryk@gmail.com>
2023-10-20 19:12:32 +0200

    [maintenance] 1.0.8-alpha.4

--- view-binder/build.gradle.kts
+++ view-binder/build.gradle.kts
@@ -40,7 +40,7 @@ afterEvaluate {
 
                 groupId = "com.github.grzegorzbobryk"
                 artifactId = "view-binder"
-                version = "1.0.8-alpha.2"
+                version = "1.0.8-alpha.4"
             }
         }
     }
