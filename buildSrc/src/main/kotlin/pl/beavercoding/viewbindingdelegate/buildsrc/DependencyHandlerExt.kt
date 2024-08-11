94360795d958b9f4cca153d03206ab50b17bc275
Grzegorz Bobryk <g.bobryk@gmail.com>
2024-08-11 18:36:31 +0200

    [release] 1.0.10 - unnecessary import removed.

--- buildSrc/src/main/kotlin/pl/beavercoding/viewbindingdelegate/buildsrc/DependencyHandlerExt.kt
+++ buildSrc/src/main/kotlin/pl/beavercoding/viewbindingdelegate/buildsrc/DependencyHandlerExt.kt
@@ -1,7 +1,6 @@
 package pl.beavercoding.viewbindingdelegate.buildsrc
 
 import org.gradle.api.artifacts.Dependency
-import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.kotlin.dsl.DependencyHandlerScope
 
